afec76f815e4e74f8ef4feacbc823477c32e03ed
= <dinhhoang210903@gmail.com>
2025-04-19 13:02:56 +0700

    (feat): add pagination

--- DTOs/CategoryDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace blog_website_api.DTOs
-{
-    public class CategoryDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = null!;
-        public string Slug { get; set; } = null!;
-    }
-}
---
+++ Helpers/Params/PaginationParams.cs
@@ -0,0 +1,13 @@
+﻿namespace blog_website_api.Helpers.Params
+{
+    public class PaginationParams
+    {
+        private int _index = 1;
+        // set pageIndex = 1 if the value set < 0
+        public int PageIndex { 
+            get => _index; 
+            set => _index = (value < 1) ? _index : value; 
+        }
+        public int PageSize { get; } = 3;
+    }
+}
